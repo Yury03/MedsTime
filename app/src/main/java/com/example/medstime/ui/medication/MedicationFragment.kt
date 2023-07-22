88d0db6168c909e5534b7fa4913a2c08c5ee050a
Yury Vishnevsky <yrvish03@bk.ru>
2023-07-22 15:37:07 +0300

    added ui for medication fragment

--- app/src/main/java/com/example/medstime/ui/medication/MedicationFragment.kt
+++ app/src/main/java/com/example/medstime/ui/medication/MedicationFragment.kt
@@ -1,23 +1,44 @@
 package com.example.medstime.ui.medication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
 import com.example.medstime.R
+import com.example.medstime.databinding.FragmentMedicationBinding
+
 
 class MedicationFragment : Fragment() {
 
-
-
-
+    private lateinit var binding: FragmentMedicationBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentMedicationBinding.inflate(inflater, container, false)
+        binding.calendar.visibility = View.VISIBLE
+        val view = binding.root
+        binding.calendar.visibility = View.GONE
+        binding.showCalendar.post { setTopMargin(binding.showCalendar.height) }
+        binding.showCalendar.setOnClickListener {
+            if (binding.calendar.visibility == View.VISIBLE) {
+                binding.calendar.visibility = View.GONE
+            } else {
+                binding.calendar.visibility = View.VISIBLE
+            }
+        }
+        return view
+    }
 
-        return inflater.inflate(R.layout.fragment_medication, container, false)
+    private fun setTopMargin(topMargin: Int) {
+        val layoutParams = binding.medicationsList.layoutParams as FrameLayout.LayoutParams
+        layoutParams.setMargins(0, topMargin, 0, 0)
+        binding.medicationsList.layoutParams = layoutParams
     }
 
 
