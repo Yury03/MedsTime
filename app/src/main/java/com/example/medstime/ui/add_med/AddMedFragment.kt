7dfbbf52d6dfcbcbde347205c9f407826ac2a33a
Yury Vishnevsky <yrvish03@bk.ru>
2023-09-08 00:20:25 +0300

    added durationLayout

--- app/src/main/java/com/example/medstime/ui/add_med/AddMedFragment.kt
+++ app/src/main/java/com/example/medstime/ui/add_med/AddMedFragment.kt
@@ -9,6 +9,9 @@ import android.widget.AutoCompleteTextView
 import androidx.fragment.app.DialogFragment
 import com.example.medstime.R
 import com.example.medstime.databinding.FragmentAddMedBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
 
 class AddMedFragment : DialogFragment() {
@@ -26,19 +29,59 @@ class AddMedFragment : DialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         setAdapterSpinDosageUnits()
         setAdapterSpinTrackType()
+        setAdapterSpinReminderType()
+        setAdapterSpinIntakeType()
+        setAdapterSpinDurationSettings()
         initView()
     }
 
     private fun initView() {
         with(binding) {
             reminderLayoutButton.setOnClickListener {
-                if ((reminderLayout.isExpanded))
-                    reminderLayout.collapse()
+                if ((reminderLayout.isExpanded)) reminderLayout.collapse()
                 else reminderLayout.expand()
             }
-
+            durationLayoutButton.setOnClickListener {
+                if (durationLayout.isExpanded) durationLayout.collapse()
+                else durationLayout.expand()
+            }
+            addIntakeTime.setOnClickListener {
+                val picker = callTimePicker(R.string.title_add_reminder)
+                picker.addOnPositiveButtonClickListener {
+                    if (picker.minute < 10)
+                        addChipTime("${picker.hour}:0${picker.minute}")
+                    else
+                        addChipTime("${picker.hour}:${picker.minute}")
+                }
+                picker.show(parentFragmentManager, "TimePicker")
+            }
         }
     }
+
+    private fun addChipTime(chipText: String) {
+        val newChip = Chip(binding.chipGroupTime.context)
+        newChip.apply {
+            text = chipText
+            setStyle(
+                com.google.android.material.R.style.Widget_MaterialComponents_TextInputLayout_OutlinedBox_ExposedDropdownMenu,
+                theme
+            )
+            isCloseIconVisible = true
+            setOnCloseIconClickListener {
+                binding.chipGroupTime.removeView(newChip)
+            }
+        }
+        binding.chipGroupTime.addView(newChip, binding.chipGroupTime.childCount)
+    }
+
+    private fun callTimePicker(textId: Int) =
+        MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText(textId)
+            .build()
+
 
     private fun setAdapterSpinDosageUnits() {
         val dosageArray = resources.getStringArray(R.array.dosage_array)
@@ -50,5 +93,23 @@ class AddMedFragment : DialogFragment() {
         val typeArray = resources.getStringArray(R.array.track_array)
         val adapter = ArrayAdapter(requireContext(), R.layout.spin_item, typeArray)
         (binding.textFieldTrackingType.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+    }
+
+    private fun setAdapterSpinReminderType() {
+        val reminderArray = resources.getStringArray(R.array.reminder_array)
+        val adapter = ArrayAdapter(requireContext(), R.layout.spin_item, reminderArray)
+        (binding.textFieldReminderType.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+    }
+
+    private fun setAdapterSpinIntakeType() {
+        val intakeArray = resources.getStringArray(R.array.intake_array)
+        val adapter = ArrayAdapter(requireContext(), R.layout.spin_item, intakeArray)
+        (binding.textFieldIntakeType.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+    }
+
+    private fun setAdapterSpinDurationSettings() {
+        val durationSettingsArray = resources.getStringArray(R.array.duration_settings_array)
+        val adapter = ArrayAdapter(requireContext(), R.layout.spin_item, durationSettingsArray)
+        (binding.textFieldDurationSettings.editText as? AutoCompleteTextView)?.setAdapter(adapter)
     }
 }
