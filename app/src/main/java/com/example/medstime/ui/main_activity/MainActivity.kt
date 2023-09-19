dafb9d96e783729faadff26dfbfd8c1f1de9e698
Yury Vishnevsky <yrvish03@bk.ru>
2023-09-19 16:25:00 +0300

    added beta mode, scan barcode ui

--- app/src/main/java/com/example/medstime/ui/main_activity/MainActivity.kt
+++ app/src/main/java/com/example/medstime/ui/main_activity/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.medstime.ui.main_activity
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.example.medstime.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +18,8 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
-        findViewById<BottomNavigationView>(R.id.bottomNavigationBar).setOnItemSelectedListener { item ->
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
+        bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.menu_medication -> navController.navigate(R.id.medicationFragment)
 
@@ -25,6 +29,24 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+        val listenerForBeta = View.OnLongClickListener { _ ->
+            val sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+            val cameraBeta = sharedPref.getBoolean(getString(R.string.sp_key_camera_beta), false)
+            with(sharedPref.edit()) {
+                if (!cameraBeta) {
+                    putBoolean(getString(R.string.sp_key_camera_beta), true)
+                    apply()
+                } else {
+                    putBoolean(getString(R.string.sp_key_camera_beta), false)
+                    apply()
+                }
+            }
+            Toast.makeText(applicationContext, R.string.beta_mode_activated, Toast.LENGTH_SHORT)
+            .show()
+            true
+        }
+        bottomNavigationView.findViewById<View>(R.id.menu_notifications)
+            .setOnLongClickListener(listenerForBeta)
     }
 
     fun hideBottomNavigationBar() {
