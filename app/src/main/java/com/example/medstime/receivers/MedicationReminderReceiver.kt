6a8fd4e39d30dd059b342a71ddd643054010bfd1
Yury Vishnevsky <yrvish03@bk.ru>
2023-10-01 16:30:04 +0300

    added buttons in notificaions

--- app/src/main/java/com/example/medstime/receivers/MedicationReminderReceiver.kt
+++ app/src/main/java/com/example/medstime/receivers/MedicationReminderReceiver.kt
@@ -2,10 +2,12 @@ package com.example.medstime.receivers
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
+import com.example.domain.models.MedicationIntakeModel
 import com.example.domain.models.ReminderModel
 import com.example.domain.usecase.reminder.ChangeNotificationStatus
 import com.example.domain.usecase.reminder.GetMedicationIntakeModel
@@ -15,6 +17,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.koin.java.KoinJavaComponent
+import java.util.Calendar
 
 
 class MedicationReminderReceiver : BroadcastReceiver() {
@@ -35,7 +38,7 @@ class MedicationReminderReceiver : BroadcastReceiver() {
         when (type) {
             ReminderModel.Type.PUSH_NOTIFICATION.toString() -> sendNotification(
                 context = context!!,
-                medicationModelID = medicationModelId
+                medicationIntakeId = medicationModelId
             )
 
             ReminderModel.Type.BANNER.toString() -> startBannerService(
@@ -57,26 +60,65 @@ class MedicationReminderReceiver : BroadcastReceiver() {
         context.startService(serviceIntent)
     }
 
-    private fun sendNotification(context: Context, medicationModelID: String) {
+    private fun sendNotification(context: Context, medicationIntakeId: String) {
         CoroutineScope(Dispatchers.IO).launch {
-            val intake = getMedicationIntakeModel.invoke(medicationModelID)
+            val intake = getMedicationIntakeModel.invoke(medicationIntakeId)
             val notificationManager =
                 context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             val channel = NotificationChannel(
                 "medication_channel",
                 "Напоминания о приеме лекарств",
-                NotificationManager.IMPORTANCE_DEFAULT
+                NotificationManager.IMPORTANCE_HIGH
             )
             notificationManager.createNotificationChannel(channel)
             val builder = NotificationCompat.Builder(context, "medication_channel")
                 .setSmallIcon(R.drawable.medication_icon_menu)
                 .setContentTitle("Напоминание о приеме лекарства ${intake.name}")
                 .setContentText("Прием назначен на ${intake.intakeTime.hour}:${intake.intakeTime.minute}")
-//            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            notificationManager.notify(1, builder.build())
+                .addAction(
+                    R.drawable.button_icon_check,
+                    context.getString(R.string.taken),
+                    getPendingIntent(context, true, medicationIntakeId)
+                )
+                .addAction(
+                    R.drawable.button_icon_skip,
+                    context.getString(R.string.skipped),
+                    getPendingIntent(context, false, medicationIntakeId)
+                )
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+            notificationManager.notify(medicationIntakeId.hashCode(), builder.build())
         }
     }
 
+    private fun getPendingIntent(
+        context: Context?,
+        isTaken: Boolean,
+        medicationIntakeId: String
+    ): PendingIntent {
+        val intent = Intent(context, ChangeIsTakenStatusReceiver::class.java)
+        intent.apply {
+            val actualTime = getActualTime()
+            putExtra("isTaken", isTaken)
+            putExtra("medicationIntakeId", medicationIntakeId)
+            putExtra("actualIntakeHour", actualTime.hour)
+            putExtra("actualIntakeMinute", actualTime.minute)
+        }
+        return PendingIntent.getBroadcast(
+            context,
+            isTaken.hashCode(),//TODO
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+    }
+
+    private fun getActualTime(): MedicationIntakeModel.Time {
+        val calendar = Calendar.getInstance()
+        return MedicationIntakeModel.Time(
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE)
+        )
+    }
 }
+
 
 
